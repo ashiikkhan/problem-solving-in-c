62826e06ccc43e8d23ef7327ff9d4e03ace741b0
ashiikkhan <ashiksust06@gmail.com>
2023-01-21 10:53:22 +0600

    xoss pattern

---
+++ Course 2 Lab Mid Term Exam/Xoss Pattern.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int n ;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) {
+            for (int j = 1; j <= n; j++) {
+                printf("%d", j);
+            }
+            printf("\n");
+        }
+        else if (i == n) { 
+            for (int j = 1; j <= n; j++) {
+                printf("%d", i);
+            }
+            printf("\n");
+        }
+        else {
+            printf("%d", i);
+            for(int j = 1; j <= n-2; j++) {
+                printf(" ");
+            }
+            printf("%d", n);
+            printf("\n");
+        }
+    }
+    return 0;
+}
--- Course 2 Lab Mid Term Exam/test.c
+++ Course 2 Lab Mid Term Exam/test.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int t;
-    scanf("%d", &t);
-    int counts[t];  // declare an array to store counts
-    for (int k = 0; k < t; k++) {
-        int n;
-        scanf("%d", &n);
-        char s[n+1];
-        scanf("%s", s);
-        int count = 0;
-        for (int i = 0; i < n-1; i++) {
-            if (s[i] == '0' && s[i+1] == '1') {
-                count++;
-            } else if (s[i] == '1' && s[i+1] == '0') {
-                count++;
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j == 1) {
+                printf("%d", i);
+            } else if (i == j) {
+                printf("%d", n);
+            } else {
+                printf(" ");
             }
         }
-        counts[k] = count; // store the count for the current test case
+        printf("\n");
     }
-    for(int i=0; i<t; i++) // print all the counts
-        printf("%d\n", counts[i]);
     return 0;
 }
