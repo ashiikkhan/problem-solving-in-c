1c01963574d5b4a0895f14b2ee00aeb446c0c151
ashiikkhan <ashiksust06@gmail.com>
2023-02-14 17:25:28 +0600

    codeforces1740A

--- problem set 22.5/[codeforces 1740A]Factorise N+M.c
+++ problem set 22.5/[codeforces 1740A]Factorise N+M.c
@@ -1,19 +1,43 @@
 //problem link: https://codeforces.com/problemset/problem/1740/A
 #include<stdio.h>
+#include<stdbool.h>
+bool is_prime(int n){
+    if(n<=1)
+        return false;
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)
+            return false;
+    }
+    return true;
+}
+int find_m(int n)
+{
+    int m=2; //2 is the first prime so starting from 2;
+    while(is_prime(n+m))
+    {
+        m++;
+        while(!is_prime(m))
+        {
+            m++;
+        }
+    }
+    return m;
+}
 int main ()
 {
-    // int t, given_prime; 
-    // scanf("%d", &t);
-    // int arr[t];
-    // for(int i=0; i<t; i++)
-    // {
-    //     scanf("%d", &given_prime);
-    // }
-    int given_prime;
-    scanf("%d", &given_prime);
-    //check korte hobe j given_prime er sathe arekta prime jeta jog korle result = not prime hobe;
-    //given_prime+prime_num!=prime ? prime_num ta array the rakhte hobe ;
-    // for(int )
+    int t, given_prime;
+    scanf("%d", &t);
+    int ans[t];
+    for(int i=0; i<t; i++)
+    {
+        scanf("%d", &given_prime);
+        ans[i]=find_m(given_prime);
+    }
 
+    for(int i=0; i<t; i++)
+    {
+        printf("%d\n", ans[i]);
+    }
     return 0;
 }
