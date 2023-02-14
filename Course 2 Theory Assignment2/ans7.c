bc6df1604e4859d2c77c0b577e50cfcc21228ad2
ashiikkhan <ashiksust06@gmail.com>
2023-02-15 00:24:18 +0600

    edit

--- Course 2 Theory Assignment2/ans7.c
+++ Course 2 Theory Assignment2/ans7.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void sort(int arr[], int n) {
     for(int i=0; i<n-1; i++)
     {
@@ -18,12 +17,9 @@ void sort(int arr[], int n) {
 double median(int arr[], int n) {
     sort(arr, n);
     if(n%2==0)
-    {
         return (double)(arr[n/2]+arr[(n/2)-1])/2.0;
-    }
-    else {
-        return (double) arr[(n/2)];
-    }
+    else 
+    return (double) arr[(n/2)];
 }
 
 int main() {
--- Course 2 Theory Assignment2/string conversion.c
+++ Course 2 Theory Assignment2/string conversion.c
@@ -3,41 +3,32 @@
 #include<stdbool.h>
 bool isCapital(char ch)
 {
-    if(ch>=65 && ch<=90){
+    if(ch>=65 && ch<=90)
         return true;
-    }
     return false;
 }
 bool isSmall(char ch)
 {
-    if(ch>=97 && ch<=122){
+    if(ch>=97 && ch<=122)
         return true;
-    }
     return false;
 }
-char toCapital(char ch)
-{
+char toCapital(char ch) {
     return ch-32;
 }
-char toSmall(char ch)
-{
+char toSmall(char ch) {
     return ch+32;
 }
-int main ()
-{
+int main () {
     char s[100];
     scanf("%s", s);
     int len = strlen(s);
     for(int i=0; i<len; i++)
     {
         if(isSmall(s[i]))
-        {
             s[i]=toCapital(s[i]);
-        }
         else if(isCapital(s[i]))
-        {
             s[i]=toSmall(s[i]);
-        }
     }
     printf("Modified String: %s\n", s);
     return 0;
